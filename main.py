74d738074ff8e47a2da84071411f5267cc3f5b18
Shutupppyouu <125131102+Shutupppyouu@users.noreply.github.com>
2025-12-23 00:41:28 -0600

    Add user text input and basic preprocessing

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+def input_lines():
+    print("Enter text here: ")
+    lines = []
+
+    while True:
+        line = input()
+        if line == "":
+            break
+        lines.append(line)
+
+    return " ".join(lines)
+
+def plain_text(text):
+    text = text.lower()
+    text = text.replace(".","")
+    text = text.replace(",","")
+    text = text.replace("!","")
+    text = text.replace("?","")
+    return text
+
 def main():
-    print("AI Text Summarizer - Initialization")
+    text = input_lines()
+    clean_text = plain_text(text)
+
+    print("\nClean Text: ")
+    print(clean_text)
 
 if __name__ == "__main__":
     main()
