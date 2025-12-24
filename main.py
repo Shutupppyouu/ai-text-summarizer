27015143bf43c705f5b77cad348916763bb1cd3a
Shutupppyouu <125131102+Shutupppyouu@users.noreply.github.com>
2025-12-24 17:49:51 -0600

    Refactor project into modular summarizer archi

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-STOP_WORDS = {
-    "a", "an", "the", "and", "or", "but", "if", "while",
-    "is", "are", "was", "were", "be", "been", "being",
-    "to", "of", "in", "on", "for", "with", "as", "by",
-    "at", "from", "that", "this", "it", "its"
-}
+from summarizer.preprocessing import preprocess_text
+from summarizer.scoring import count_words, score_senteces
+from summarizer.extractive import select_top_sentences
 
 def input_lines():
     print("Enter text here: ")
@@ -17,77 +14,12 @@ def input_lines():
 
     return " ".join(lines)
 
-def preprocess_text(text):
-    text = text.lower()
-    
-    punctuation = ".,!?;:"
-    for char in punctuation:
-        text = text.replace(char, "")
-    
-    words = text.split()
-
-    filtered_words = []
-    for word in words:
-        if word not in STOP_WORDS:
-            filtered_words.append(word)
-
-    return " ".join(filtered_words)
-
-
 def split_into_sentences(text):
     #split text into sentences using periods
     sentences = text.split(".")
     #Remove empty sentences and remove whitespace
     sentences = [s.strip() for s in sentences if s.strip() != ""]
     return sentences
-
-def count_words(text):
-    words = text.split()
-    freq = {}
-
-    for word in words:
-        if word in freq:
-            freq[word] += 1
-        else:
-            freq[word] = 1
-    
-    return freq
-
-def score_senteces(sentences, word_freq):
-    scores = {}
-    for sentence in sentences:
-        words = sentence.split()
-
-        if len(words) == 0:
-            continue
-
-        score = 0
-        for word in words:
-            if word in word_freq:
-                score += word_freq[word]
-        scores[sentence] = score / len(words)
-
-    return scores
-
-def select_top_sentences(sentence_scores, original_sentences, n = 2):
-    #sort sentences by score (high to low)
-    sorted_sentences = sorted(
-        sentence_scores.items(),
-        key = lambda item: item[1],
-        reverse = True
-    )
-
-    #select top N sentences
-    top_sentences = [sentence for sentence, score in sorted_sentences[:n]]
-
-    #preserve the original order
-    ordered_summary = []
-    for sentence in original_sentences:
-        clean_sentence = preprocess_text(sentence)
-        if clean_sentence in top_sentences:
-            ordered_summary.append(sentence)
-
-    return ordered_summary
 
 def main():
     text = input_lines()
