12ef668bc274fd11fda082808860ff3464841ead
Shutupppyouu <125131102+Shutupppyouu@users.noreply.github.com>
2025-12-24 00:01:49 -0600

    Add extractive text summarization

--- main.py
+++ main.py
@@ -49,6 +49,25 @@ def score_senteces(sentences, word_freq):
 
     return scores
 
+def select_top_sentences(sentence_scores, original_sentences, n = 2):
+    #sort sentences by score (high to low)
+    sorted_sentences = sorted(
+        sentence_scores.items(),
+        key = lambda item: item[1],
+        reverse = True
+    )
+
+    #select top N sentences
+    top_sentences = [sentence for sentence, score in sorted_sentences[:n]]
+
+    #preserve the original order
+    ordered_summary = []
+    for sentence in original_sentences:
+        clean_sentence = plain_text(sentence)
+        if clean_sentence in top_sentences:
+            ordered_summary.append(sentence)
+
+    return ordered_summary
 
 def main():
     text = input_lines()
@@ -62,9 +81,11 @@ def main():
     word_freq = count_words(clean_text)
     sentence_scores = score_senteces(clean_sentences, word_freq)
     
-    print("\nSentence Scores")
-    for sentence, score in sentence_scores.items():
-        print(f"{sentence} -> {score}")
+    summary = select_top_sentences(sentence_scores, sentences, n=2)
+
+    print("\nSummary:")
+    for s in summary:
+        print(s)
 
 
 if __name__ == "__main__":
